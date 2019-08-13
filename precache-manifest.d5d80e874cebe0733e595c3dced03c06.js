10e748fa0270bce841af470f2aff267fb0326139
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 15:05:33 +0100

    Updates

---
+++ precache-manifest.d5d80e874cebe0733e595c3dced03c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab9f77b0966640765d5b2cab42f2f64",
+    "url": "/weeksix/index.html"
+  },
+  {
+    "revision": "bd435589d06b2dd82538",
+    "url": "/weeksix/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "33c03e170e6cc4119415",
+    "url": "/weeksix/static/js/2.45949301.chunk.js"
+  },
+  {
+    "revision": "bd435589d06b2dd82538",
+    "url": "/weeksix/static/js/main.83b01245.chunk.js"
+  },
+  {
+    "revision": "5b3cce33730bf0fa3b1a",
+    "url": "/weeksix/static/js/runtime~main.7d0f2a03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weeksix/static/media/logo.5d5d9eef.svg"
+  }
+]);
